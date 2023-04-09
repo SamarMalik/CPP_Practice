c7ff923cb8a63f4580e7092e94399abbde340029
Samar Malik <130270499+SamarMalik@users.noreply.github.com>
2023-04-09 10:47:21 +0500

    Create Multiplication.cpp

---
+++ Multiplication.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int valueOne;
+	int valueTwo;
+	
+	cout<<"Please enter value number one ";
+	cin>>valueOne;
+	
+	cout<<"Please enter value number two";
+	cion>>valueTwo;
+	
+	cout<<valueOnexvalueTwo;
+}
+
