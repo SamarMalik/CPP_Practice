5e9108ad75d7479b30f0c69e49c6d9acf057ef3a
Muhammad Aqeel <130394025+AqeelAbidi@users.noreply.github.com>
2023-04-10 23:41:18 +0500

    Update add.cpp
    Add some new codes..

--- add.cpp
+++ add.cpp
@@ -9,8 +9,20 @@ int main()
 	cin>>valueOne;
 	
 	cout<<"Please enter value number two";
-	cion>>valueTwo;
+	cin>>valueTwo;
 	
+	cout<<"please select the choice";
+	cout<<"1) add\n2) sub";
+	int choice;
+	cin>>choice;
+	
+	if(choice==1)
+	{
 	cout<<valueOne+valueTwo;
+	}
+	else if(choice==2)
+	{
+        cout<<valueOne-valueTwo;
+	}
 }
 
